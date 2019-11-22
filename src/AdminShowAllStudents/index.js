e1fc2442a87cddd536a899c3244c1502fb1ab21b
Kristiana Blake <kristiana.blake@gmail.com>
2019-11-21 19:47:22 -0600

    AdminShowAllStudents updated

--- src/AdminShowAllStudents/index.js
+++ src/AdminShowAllStudents/index.js
@@ -1,37 +1,22 @@
 import React, { Component } from "react";
-import { Form } from "semantic-ui-react";
+// import { Form } from "semantic-ui-react";
 
-class AdminShowAllStudents extends Component {
-	constructor(props) {
-		super()
+function AdminShowAllStudents(props){
 
-		this.state = {};
-		// sets padawans in state 
-		padawans: [],
-		// set courses in state 
-		courses: []
-	}
+	const padawans = props.padawans.map((padawan) => {
+		return (
 
-	render() {
-		return(
-			// use function instead? 
-			// if padawans are enrolled in courses, display in in component 
-			// const enrolledStudents = props.padawans.map((padawan) => {
-				// return
-			// })
-
-			// get the students that are registered (enrolled) 
-				// make an AJAX call from react to flask to get the list of all the students 
-				// hit the route the route within in React 
-
-			// loop through all of the students
-			// display them on the screen
+			<li key={padawan.id}> {padawan.full_name} </li>
 
 
-			)
-	}
+		)
+	})
+
+	return(
+
+		<ul> { padawans } </ul>
+
+	)
 }
 
-
-
-Export default AdminShowAllStudents 
+export default AdminShowAllStudents 
