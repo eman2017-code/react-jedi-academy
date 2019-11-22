0d173a8eceb7c733a2032dbd6d3f78e124b31cd0
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-11-22 12:31:02 -0600

    fixing conflict

--- src/AdminContainer/index.js
+++ src/AdminContainer/index.js
@@ -24,7 +24,7 @@ class AdminContainer extends Component {
 		try{
 
 			const padawans = await fetch(process.env.REACT_APP_API_URL + '/api/v1/padawans')
-			const parsedPadawans = wait padawans.json();
+			const parsedPadawans = await padawans.json();
 			console.log(parsedPadawans);
 
 			this.setState({
