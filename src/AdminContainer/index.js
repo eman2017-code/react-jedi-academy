f5c3ca29c9c298719f33eb1bd10b70a932742080
Kristiana Blake <kristiana.blake@gmail.com>
2019-11-22 14:48:12 -0600

    credentials added to admin view students

--- src/AdminContainer/index.js
+++ src/AdminContainer/index.js
@@ -24,7 +24,9 @@ class AdminContainer extends Component {
 
 		try{
 
-			const padawans = await fetch(process.env.REACT_APP_API_URL + '/api/v1/padawans')
+			const padawans = await fetch(process.env.REACT_APP_API_URL + '/api/v1/padawans', {
+				credentials: "include"
+			})
 			const parsedPadawans = await padawans.json();
 			console.log(parsedPadawans);
 
