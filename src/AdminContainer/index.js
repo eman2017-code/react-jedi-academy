c7edec1b63862638760069a504f207e5ed416ac8
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-11-22 18:03:58 -0600

    fixed delete route

--- src/AdminContainer/index.js
+++ src/AdminContainer/index.js
@@ -1,95 +1,94 @@
-import React, { Component } from 'react';
-import AdminShowAllStudents from '../AdminShowAllStudents';
-import AdminShowAllCourses from '../AdminShowAllCourses';
-import AdminAddCourse from '../AdminAddCourse';
+import React, { Component } from "react";
+import AdminShowAllStudents from "../AdminShowAllStudents";
+import AdminShowAllCourses from "../AdminShowAllCourses";
+import AdminAddCourse from "../AdminAddCourse";
 import { Button } from "semantic-ui-react";
 
-
 class AdminContainer extends Component {
-	constructor(props){
-		super(props)
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			padawans: [],
-			courses: [],
-			addCourse: false
+    this.state = {
+      padawans: [],
+      courses: [],
+      addCourse: false
+    };
+  }
 
-		}
-	}
+  componentDidMount() {
+    this.getStudents();
+    this.getCourses();
+  }
 
-	componentDidMount(){
-		this.getStudents();
-		this.getCourses();
-	}
+  getStudents = async () => {
+    try {
+      const padawans = await fetch(
+        process.env.REACT_APP_API_URL + "/api/v1/padawans/",
+        {
+          credentials: "include"
+        }
+      );
+      const parsedPadawans = await padawans.json();
+      console.log(parsedPadawans);
 
-	getStudents = async () => {
+      this.setState({
+        padawans: parsedPadawans.data
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-		try{
+  getCourses = async () => {
+    try {
+      const courses = await fetch(
+        process.env.REACT_APP_API_URL + "/api/v1/courses/",
+        {
+          credentials: "include"
+        }
+      );
 
-			const padawans = await fetch(process.env.REACT_APP_API_URL + '/api/v1/padawans/', {
-				credentials: "include"
-			});
-			const parsedPadawans = await padawans.json();
-			console.log(parsedPadawans);
+      const parsedCourses = await courses.json();
+      console.log(parsedCourses);
 
+      this.setState({
+        courses: parsedCourses.data
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  deleteCourse = async id => {
+    const deleteCourseResponse = await fetch(
+      process.env.REACT_APP_API_URL + "/api/v1/courses/" + id,
+      {
+        credentials: "include",
+        method: "DELETE"
+      }
+    );
 
-			this.setState({
-				padawans: parsedPadawans.data 
-			})
+    const deleteCourseParsed = await deleteCourseResponse.json();
+    console.log(deleteCourseParsed);
+    this.setState({
+      courses: this.state.courses.filter(course => course.id !== id)
+    });
+  };
 
-		} catch(err) {
-			console.log(err);
-		}
-		
-	}
+  render() {
+    return (
+      <div>
+        <div>{this.state.addCourse ? <AdminAddCourse /> : null}</div>
 
-	getCourses = async () => {
-
-		try {
-			const courses = await fetch(process.env.REACT_APP_API_URL + '/api/v1/courses/', {
-				credentials: "include"
-			});
-
-			const parsedCourses = await courses.json();
-					console.log(parsedCourses)
-
-			this.setState({
-				courses: parsedCourses.data
-			})
-			
-		}
-		catch (err) {
-		console.log(err)
-		}
-		
-	};
-
-	deleteCourse = async (id) =>{
-
-		const deleteCourseResponse = await fetch(process.env.REACT_APP_API_URL + '/api/v1/courses/' + id, { method: 'DELETE' });
-
-		const deleteCourseParsed = await deleteCourseResponse.json();
-		console.log(deleteCourseParsed);
-		this.setState({courses: this.state.courses.filter((course) => course.id !== id )})
-
-	}
-
-	
-	render(){
-		return(
-			<div>
-			<div>{this.state.addCourse ? <AdminAddCourse/> : null}</div>
-				
-				<AdminShowAllStudents padawans={this.state.padawans} />
-				<AdminShowAllCourses courses={this.state.courses} deleteCourse={this.deleteCourse} />
-				<Button onClick={this.addCourse}>Add A Course</Button>
-			</div> 
-		)
-	}
-
-
+        <AdminShowAllStudents padawans={this.state.padawans} />
+        <AdminShowAllCourses
+          courses={this.state.courses}
+          deleteCourse={this.deleteCourse}
+        />
+        <Button onClick={this.addCourse}>Add A Course</Button>
+      </div>
+    );
+  }
 }
 
-export default AdminContainer
-
+export default AdminContainer;
