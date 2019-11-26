6ccdbe10eaf32c34eaea513f83cba68f41598c00
Emmanuel Miller <emmanuelmiller355@gmail.com>
2019-11-26 09:35:13 -0600

    different pic for padawan avatar

--- src/PadawanCourseList/index.js
+++ src/PadawanCourseList/index.js
@@ -43,12 +43,12 @@ class PadawanCourseList extends Component {
   render() {
     return (
       <div className="ui centered cards">
-      <Header as="h2">JEDI IN TRAINING</Header>
+        <Header as="h2">JEDI IN TRAINING</Header>
         <div>
           <div>
             <Card>
               <Image
-                src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
+                src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/27d50ee6-198c-4501-b5fc-ce874c1d84cf/d12iveq-33f8340e-7374-478c-88dd-4019b6d2efbb.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3ZDUwZWU2LTE5OGMtNDUwMS1iNWZjLWNlODc0YzFkODRjZlwvZDEyaXZlcS0zM2Y4MzQwZS03Mzc0LTQ3OGMtODhkZC00MDE5YjZkMmVmYmIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.vWyqAXph7jtjXhGaSRORIoOufZWIgXKTbdxBOkzFwNE"
                 wrapped
                 ui={false}
               />
--- src/PadawanDashboard/index.js
+++ src/PadawanDashboard/index.js
@@ -14,12 +14,12 @@ class PadawanDashboard extends Component {
 
   render() {
     return [
-    <div>
+      <div>
         <div>
           <Header as="h2">
             <Image
               circular
-              src="https://react.semantic-ui.com/images/avatar/large/patrick.png"
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/27d50ee6-198c-4501-b5fc-ce874c1d84cf/d12iveq-33f8340e-7374-478c-88dd-4019b6d2efbb.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3ZDUwZWU2LTE5OGMtNDUwMS1iNWZjLWNlODc0YzFkODRjZlwvZDEyaXZlcS0zM2Y4MzQwZS03Mzc0LTQ3OGMtODhkZC00MDE5YjZkMmVmYmIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.vWyqAXph7jtjXhGaSRORIoOufZWIgXKTbdxBOkzFwNE"
             />{" "}
             JEDI ACADEMY - PADAWAN DASHBOARD
           </Header>
@@ -34,24 +34,24 @@ class PadawanDashboard extends Component {
           verticalAlign="top"
           stackable
         >
-        <Grid.Column>
-        <PadawanCourseList
-          key="one"
-          loggedInPadawan={this.state.loggedInPadawan}
-        />,
-        </Grid.Column>
-        <Grid.Column>
-        <Header as="h2">ENROLLMENT OPTIONS</Header>
-        <PadawanEnrollment
-          key="two"
-          loggedInPadawan={this.state.loggedInPadawan}
-        />,
-        <Button key="three" onClick={this.props.padawanLogOut}>
-          Log Out
-        </Button>
-        </Grid.Column>
-
-
+          <Grid.Column>
+            <PadawanCourseList
+              key="one"
+              loggedInPadawan={this.state.loggedInPadawan}
+            />
+            ,
+          </Grid.Column>
+          <Grid.Column>
+            <Header as="h2">ENROLLMENT OPTIONS</Header>
+            <PadawanEnrollment
+              key="two"
+              loggedInPadawan={this.state.loggedInPadawan}
+            />
+            ,
+            <Button key="three" onClick={this.props.padawanLogOut}>
+              Log Out
+            </Button>
+          </Grid.Column>
         </Grid>
       </div>
     ];
